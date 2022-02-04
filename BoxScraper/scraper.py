e9d315c5b0e20d4962909b93193a2b0ef6e3f450
BrokenShell <webmaster@sharpdesigndigital.com>
2022-02-04 07:44:48 -0800

    fixes file not found bug

--- BoxScraper/scraper.py
+++ BoxScraper/scraper.py
@@ -12,40 +12,35 @@ box = BoxWrapper()
 mongo = Mongo()
 s3 = S3()
 
-print("\nInitializing Scraper\n")
-finished_data = mongo.connect().find({}, {"_id": False, "box_id": True})
-finished = {record["box_id"] for record in finished_data}
 
-
-def scrape_box(folder_id="0"):
+def scrape_box(finished_ids, folder_id="0"):
     folder_info = box.client.folder(folder_id).get()
-    print(f"Scanning: {folder_info['name']}")
+    print(f"Scanning Folder: {folder_id}, {folder_info['name']}")
     files, folders = box.items_in_folder(folder_id)
-
     for file in files:
-        if file["id"] not in finished:
-            finished.add(file["id"])
+        if file["id"] not in finished_ids:
+            finished_ids.add(file["id"])
             scrape_file(file["id"])
     for folder in folders:
-        scrape_box(folder["id"])
+        scrape_box(finished_ids, folder["id"])
 
 
 def scrape_file(file_id):
     info = box.get_file_info(file_id)
     if info["ext"] == "pdf":
-        print(f"Scraping: {info['name']}")
+        print(f"Scraping File: {info['id']}, {info['name']}")
         print(" -Downloading File")
         pdf_bytes = box.download_file(file_id)
-        print(" -Generating Thumbnail")
         file_name = f"{file_id}.jpg"
-        file_path = os.path.join("thumbs", file_name)
+        print(" -Generating Thumbnail")
+        file_path = os.path.join("BoxScraper", "thumbs", file_name)
         thumbnail = get_thumbnail(pdf_bytes)
         thumbnail.save(file_path, format="jpeg")
         print(" -Uploading Thumbnail")
         bucket_name = "docdb-thumbnails"
         s3.upload(file_path, bucket_name, file_name)
         print(" -Processing Text")
-        ocr_text = ocr(pdf_bytes, dpi=300)
+        ocr_text = ocr(pdf_bytes, dpi=150)
         record = {
             "box_id": info["id"],
             "name": info["name"],
@@ -55,9 +50,13 @@ def scrape_file(file_id):
             "summary": summary(ocr_text),
             "text": ocr_text,
         }
-        print(f" -Inserting Data: {file_id}")
+        print(" -Inserting Data")
         mongo.insert(record)
 
 
 if __name__ == '__main__':
-    scrape_box()
+    print("\nInitializing BoxScraper...", end=" ")
+    data = mongo.connect().find({}, {"_id": False, "box_id": True})
+    finished_data = {record["box_id"] for record in data}
+    print("Ready!\n")
+    scrape_box(finished_data, folder_id="129133191949")
